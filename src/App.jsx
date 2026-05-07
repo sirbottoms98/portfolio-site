d0cd2b69fd1787eb769cb6b2a6a9d4d8b422b64c
Patrick Paul <patrickpaul0918@gmail.com>
2026-05-07 13:05:51 -0500

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div>
+        <h1>Patrick Paul</h1>
+        <p>Embedded Systems • CE/EE Student • Programmer</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>PatrickPaul.dev</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
